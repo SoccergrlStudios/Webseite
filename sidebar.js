2f447c0bdb2b285c2f09f47ca13f48722eec4806
SoccergrlStudios <soccergrl.studios@gmail.com>
2025-11-06 23:00:41 +0100

    Overview Pages
    Created Pages index, apps, games, minecrafplugins and contact

---
+++ sidebar.js
@@ -0,0 +1,22 @@
+﻿var oldArticleHeight = 0;
+
+function openSidebar() {
+    document.getElementById("sidebar").style.display = "block";
+
+    oldArticleHeight = document.getElementById("article").offsetHeight;
+    if(document.getElementById("article").offsetHeight < document.getElementById("sidebar").offsetHeight){
+        document.getElementById("article").style.height = document.getElementById("sidebar").offsetHeight + "px";
+    }
+
+    document.getElementById("buttonOpen").style.marginTop = "0.5em";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.display = "none";
+
+    if(oldArticleHeight !== document.getElementById("article").offsetHeight){
+        document.getElementById("article").style.height = oldArticleHeight + "px";
+    }
+
+    document.getElementById("buttonOpen").style.marginTop = "";
+}
